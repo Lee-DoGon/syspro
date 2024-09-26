ab006af56a9e22a169286cd6edcbe5e83a2fae38
ydg6636 <ydg6636@naver.com>
2024-09-26 07:55:34 +0000

    .

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -2,23 +2,38 @@
 #include <string.h>
 #include "copy.h"
 
-char line[MAXLINE]; // input line
-char longest[MAXLINE]; // longest line
+char line[5][MAXLINE];
+char longest[MAXLINE];
 
 int main(void)
 {
 	int len, max = 0;
 
-	
-	while (fgets(line, MAXLINE, stdin) != NULL)
+	int i = 0;
+
+
+	while (i < 5)
 	{
+		fgets(line[i], MAXLINE, stdin);
+
+		/*
+		if (strlen(line[i]) > strlen(line[i+1]))
+		{
+			strcpy(longest, line[i+1]);
+			strcpy(line[i+1], line[i]);
+			strcpy(line[i], longest);
+		}
+		*/
+
+		/*
 		len = strlen(line);
 
 		if (len > max)
 		{
 			max = len;
-			copy(line, longest);
+			copy(line, longest[i]);
 		}
+		*/
 
 		i++;
 	}
@@ -26,9 +41,22 @@ int main(void)
 
 	for (int i = 0; i < 5; i++)
 	{
-		printf("%s", longest);
+		for (int j = 0; i < 5; j++)
+		{
+		
+			if (strlen(line[i]) > strlen(line[i+1]))
+			{
+				strcpy(longest, line[i+1]);
+				strcpy(line[i+1], line[i]);
+				strcpy(line[i], longest);
+			}
+		}
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%s", line[i]);
 	}
 
 	return 0;
-
 }
