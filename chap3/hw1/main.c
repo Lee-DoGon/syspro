4dc5a9df3e1d174150e4c03f5a47411462d0cfa5
ydg6636 <ydg6636@naver.com>
2024-10-10 01:33:59 +0000

    .

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -7,29 +7,19 @@ char longest[MAXLINE];
 
 int main(void)
 {
-	// int len, max = 0;
-
 	for (int i = 0; i < 5; i++)
 	{
 		fgets(line[i], MAXLINE, stdin);
-
-		// scanf("%s", line[i]);
-		// len = strlen(line[i]);
-
-		/*
-		if (len > max)
-		{
-			max = len;
-			copy(line[i], longest);
-		}
-		*/
 	}
-
 	
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = i + 1; j < 5; j++)
 		{
+			// strcmp, string comparison
+			// if return val < 0 _ str1 < str2
+			// if val == 0 _ str1 == str2
+			// val > 0 _ str1 > str2
 			if (strcmp(line[i],line[j]) > 0)
 			{
 				strcpy(longest, line[i]);
