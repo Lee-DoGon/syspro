c4f472081cbff51b4cee2b1857e8130b8b4ebda3
ydg6636 <144796016+Lee-DoGon@users.noreply.github.com>
2024-10-17 15:28:34 +0900

    Update main.c

---
+++ chap6/prob6/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (link(argv[1], argv[2]) == -1)
+    exit(1);
+
+  exit(0);
+}
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) { // 수정: 인자 수 확인
+        fprintf(stderr, "Usage: %s <source> <link>\n", argv[0]);
+        exit(1);
+    }
+
+    if (link(argv[1], argv[2]) == -1) {
+        perror("link"); // 수정: 오류 메시지 출력
+        exit(1);
+    }
+
+    exit(0);
+}
+*/
