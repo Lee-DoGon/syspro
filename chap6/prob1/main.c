3cc485e224f306ebe704f46dde7b4307f94e2917
ydg6636 <144796016+Lee-DoGon@users.noreply.github.com>
2024-10-17 14:51:02 +0900

    Update main.c

--- chap6/prob1/main.c
+++ chap6/prob1/main.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    struct stat buf;
+
+    for (i = 1; i < argc; i++)
+    {
+        printf("%s: ", argv[i]);
+
+        if (lstat(argv[i], &buf) < 0)
+        {
+            perror("lstat()");
+            continue;
+        }
+
+        if (S_ISREG(buf.st_mode))
+            printf("Regular file\n");
+        else if (S_ISDIR(buf.st_mode))
+            printf("Directory\n");
+        else if (S_ISCHR(buf.st_mode))
+            printf("Character device\n");
+        else if (S_ISFIFO(buf.st_mode))
+            printf("FIFO (named pipe)\n");
+        else if (S_ISLNK(buf.st_mode))
+            printf("Symbolic link\n");
+        else if (S_ISSOCK(buf.st_mode))
+            printf("Socket\n");
+        else
+            printf("Other type\n");
+    }
+
+    exit(0);
+}
+
+
+/*
+#include <stdio.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -35,3 +76,4 @@ int main(int argc, char *argv[])
 
 	exit(0);
 }
+*/
