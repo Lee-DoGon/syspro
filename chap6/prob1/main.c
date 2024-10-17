24edcef66c2f71220547369e863a6e909dd5b4f4
ydg6636 <ydg6636@naver.com>
2024-10-17 05:49:16 +0000

    .

---
+++ chap6/prob1/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct stat buf;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s: ",argv[i]);
+
+		if (lstat(argv[i], &buf) < 0)
+		{
+			perror("lstat()");
+			continue;
+		}
+
+		if (S_ISREG(buf.st_mode))
+			printf("%s \n", file);
+		if (S_ISDIR(buf.st_mode))
+			printf("%s \n", div);
+		if (S_ISCHR(buf.st_mode))
+			printf("%s \n", chdir);
+		if (S_ISFIFO(buf.st_mode))
+			printf("%s \n", mkfifo);
+		if (S_ISLNK(buf.st_mode))
+			printf("%s \n", link);
+		if (S_ISSOCK(buf.st_mode))
+			printf("%s \n", sock);
+	}
+
+	exit(0);
+}
