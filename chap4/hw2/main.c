2d28d3e668157a9ddb9aa15450b3006ad9b0683d
ydg6636 <ydg6636@naver.com>
2024-10-18 03:35:22 +0000

    .

--- chap4/hw2/main.c
+++ chap4/hw2/main.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printFile(FILE *fp, int lineNumP)
+{
+	int c;
+	int lineNum = 1;
+
+	if (lineNumP)
+		printf("%d: ", lineNum);
+
+	while ((c = getc(fp)) != EOF)
+	{
+		putc(c, stdout);
+
+		if (c == '\n')
+		{
+			lineNum++;
+
+			if (lineNumP)
+				printf("%d: ", lineNum);
+		}
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
 	int c;
+	int lineNum = 0;
 
-	if (argc < 2)
-		fp = stdin;
-	else
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], '\n') == 0)
+		{
+			lineNum = 1;
+		}
+//		else
+//		{
+//			if (i > 1)
+//			printf("\n=== %s ===\n", argv[i]);
+//		}
+
 		fp = fopen(argv[1], "r");
 
-	c = getc(fp);
+		if (fp == NULL)
+			return 1;
 
-	while (c != EOF)
-	{
-		putc(c, stdout);
-		c = getc(fp);
+		printFile(fp, lineNum);
+
+		fclose(fp);
 	}
-
-	fclose(fp);
-
-	return 0;
 }
