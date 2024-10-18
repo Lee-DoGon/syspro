ce3e96e85cdb9d99199743748884a681d60e4d28
ydg6636 <144796016+Lee-DoGon@users.noreply.github.com>
2024-10-18 12:14:30 +0900

    Update main.c

--- chap4/prob2/main.c
+++ chap4/prob2/main.c
@@ -2,34 +2,37 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
-	FILE *fp1, *fp2;
+    char c;
+    FILE *fp1, *fp2;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "how to use : %s File1 File2\n", argv[0]);
-		
-		return 1;
-	}
+    if (argc != 3)
+    {
+        fprintf(stderr, "how to use : %s File1 File2\n", argv[0]);
+        return 1;
+    }
 
+    fp1 = fopen(argv[1], "r");
+    if (fp1 == NULL)
+    {
+        fprintf(stderr, "File %s Open Error\n", argv[1]);
+        return 2;
+    }
 
-	fp1 = fopen(argv[1], "r");
+    fp2 = fopen(argv[2], "w");
+    if (fp2 == NULL)
+    {
+        fprintf(stderr, "File %s Open Error\n", argv[2]);
+        fclose(fp1);
+        return 3;
+    }
 
-	if (fp1 == NULL)
-	{
-		fprintf(stderr, "File %s Open Error", argv[1]);
+    while ((c = fgetc(fp1)) != EOF)
+    {
+        fputc(c, fp2);
+    }
 
-		return 2;
-	}
+    fclose(fp1);
+    fclose(fp2);
 
-
-	fp2 = fopen(argv[2], "w");
-
-	while ((c = fgetc(fp1)) != EOF)
-		fputc(c , fp2);
-
-	fclose(fp1);
-	fclose(fp1);
-
-	return 0;
+    return 0;
 }
