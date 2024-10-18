9a65451ed9e737a85e2edff54e3c68070d63236b
ydg6636 <ydg6636@naver.com>
2024-10-18 12:38:40 +0000

    up ch6 p11

--- chap6/prob11/main.c
+++ chap6/prob11/main.c
@@ -10,31 +10,50 @@
 
 char type(mode_t);
 char *perm(mode_t);
-void printStat(char *, char *, struct stat *);
+void printStat(char *, char *, struct stat *, int, int, int);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   DIR *dp;
-  char *dir;
+  char *dir = ".";
   struct dirent *d;
   struct stat st;
   char path[BUFSIZ + 1];
 
-  if (argc == 1)
-    dir = ".";
-  else
-	  dir = ".";
+  int op_i = 0;
+  int op_p = 0;
+  int op_q = 0;
+
+
+  for (int i = 1; i < argc; i++)
+  {
+	  if (strcmp(argv[i], "-i") == 0)
+		  op_i = 1;
+	  else if (strcmp(argv[i], "-p") == 0)
+		  op_p = 1;
+	  else if (strcmp(argv[i], "-Q") == 0)
+		  op_q = 1;
+	  else
+		  dir = argv[i];
+  }
+
+//  if (argc == 2)
+//   dir = ".";
+//  else
+//	  dir = ".";
 //    dir = argv[1];
 
   if ((dp = opendir(dir)) == NULL) // dir open
     perror(dir);
 
-  while ((d = readdir(dp)) != NULL) {
+  while ((d = readdir(dp)) != NULL)
+  {
     sprintf(path, "%s/%s", dir, d->d_name);
 
     if (lstat(path, &st) < 0)
       perror(path);
     else
-      printStat(path, d->d_name, &st);
+      printStat(path, d->d_name, &st, op_i, op_p, op_q);
   }
 
   closedir(dp);
@@ -42,18 +61,30 @@ int main(int argc, char *argv[]) {
   exit(0);
 }
 
-void printStat(char *pathname, char *file, struct stat *st) {
-  printf("%5ld ", st->st_blocks);
-  printf("%c%s ", type(st->st_mode), perm(st->st_mode));
-  printf("%3ld ", st->st_nlink);
-  printf("%s %s ", getpwuid(st->st_uid)->pw_name,
-         getgrgid(st->st_gid)->gr_name);
-  printf("%9ld ", st->st_size);
-  printf("%.12s ", ctime(&st->st_mtime) + 4);
-  printf("%s\n", file);
+
+void printStat(char *pathname, char *file, struct stat *st, int op_i, int op_p, int op_q)
+{
+	if (op_i)
+		printf("%ld ", st->st_ino);
+
+	printf("%c%s ", type(st->st_mode), perm(st->st_mode));
+
+	if (op_p && S_ISDIR(st->st_mode))
+		printf("%s/ ", file);
+	else 
+	{
+		if (op_q)
+			printf("\"%s\" ", file);
+		else
+			printf("%s ", file);
+	}
+
+	printf("\n");
 }
 
-char type(mode_t mode) {
+
+char type(mode_t mode)
+{
   if (S_ISREG(mode))
     return ('-');
   if (S_ISDIR(mode))
@@ -70,11 +101,14 @@ char type(mode_t mode) {
     return ('s');
 }
 
-char *perm(mode_t mode) {
+
+char *perm(mode_t mode)
+{
   static char perms[10];
   strcpy(perms, "--------");
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++)
+  {
     if (mode & (S_IRUSR >> i * 3))
       perms[i * 3] = 'r';
 
