1b46b4be3ce83168731a9be408d7cf7006673326
ydg6636 <144796016+Lee-DoGon@users.noreply.github.com>
2024-11-07 13:52:55 +0900

    Update kwazae.c

--- chap8/hw1/kwazae.c
+++ chap8/hw1/kwazae.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
@@ -14,85 +13,76 @@
 
 int main(int argc, char *argv[])
 {
-	int op_e = 0;
-	int op_u = 0; // pro real user id
-	int op_g = 0; // pro real group id
-	int op_i = 0; // pro id
-	int op_p = 0; // parent pro id
+    int op_e = 0;
+    int op_u = 0; // pro real user id
+    int op_g = 0; // pro real group id
+    int op_i = 0; // pro id
+    int op_p = 0; // parent pro id
+    char *env_var = NULL;
 
-	for (int i = 1; i < argc; i++)
-	{
-		if (strcmp(argv[i], "-e") == 0)
-			op_e = 1;
-		else if (strcmp(argv[i], "-u") == 0)
-			op_u = 1;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-e") == 0)
+        {
+            op_e = 1;
+            if (i + 1 < argc) // cheak after -e off
+            {
+                env_var = argv[i + 1];
+                i++;
+            }
+        }
+        else if (strcmp(argv[i], "-u") == 0)
+            op_u = 1;
+        else if (strcmp(argv[i], "-g") == 0)
+            op_g = 1;
+        else if (strcmp(argv[i], "-i") == 0)
+            op_i = 1;
+        else if (strcmp(argv[i], "-p") == 0)
+            op_p = 1;
+    }
 
-		else if (strcmp(argv[i], "-g") == 0)
-			op_g = 1;
+    ////////////////////
+    if (op_e == 1)
+    {
+        if (env_var != NULL)
+        {
+            char *value = getenv(env_var);
+            if (value != NULL)
+                printf("%s=%s\n", env_var, value);
+            else
+                printf("no %s\n", env_var);
+        }
+        else
+        {
+            char **ptr;
+            extern char **environ;
 
-		else if (strcmp(argv[i], "-i") == 0)
-			op_i = 1;
+            for (ptr = environ; *ptr != 0; ptr++)
+                printf("%s\n", *ptr);
+        }
 
-		else if (strcmp(argv[i], "-p") == 0)
-			op_p = 1;
-	}
+        exit(0);
+    }
+    else if (op_u == 1)
+    {
+        printf("my realistic user id : %d(%s)\n", getuid(), getpwuid(getuid())->pw_name);
+        printf("my valid user id : %d(%s)\n", geteuid(), getpwuid(geteuid())->pw_name);
+    }
+    else if (op_g == 1)
+    {
+        printf("my realistic group id : %d(%s)\n", getgid(), getgrgid(getgid())->gr_name);
+        printf("my valid group id : %d(%s)\n", getgid(), getgrgid(getuid())->gr_name);
+    }
+    else if (op_i == 1)
+    {
+        printf("my process number : [%d]\n", getpid());
+        exit(0);
+    }
+    else if (op_p == 1)
+    {
+        printf("my parent's process number : [%d]\n", getppid());
+        exit(0);
+    }
 
-	////////////////////
-	if (op_e == 1)
-	{
-		char **ptr;
-		extern char **environ;
-
-		for (ptr = environ; *ptr != 0; ptr++)
-			printf("%s\n", *ptr);
-
-		exit(0);
-	}
-	else if (op_u == 1)
-	{
-		printf("my realistic user id : %d(%s)\n", getuid(), getpwuid(getuid())->pw_name);
-		printf("my valid user id : %d(%s)\n", geteuid(), getpwuid(geteuid())->pw_name);
-	}
-	else if (op_g == 1)
-	{
-	printf("my realistic group id : %d(%s)\n", getgid(), getgrgid(getgid())->gr_name);
-	printf("my valid group id : %d(%s)\n", getgid(), getgrgid(getuid())->gr_name);
-	}
-	else if (op_i == 1)
-	{
-		printf("my process number : [%d]\n", getpid());
-
-		exit(0);
-	}
-	else if (op_p == 1)
-	{
-		printf("my parent's process number : [%d]\n", getppid());
-
-		exit(0);
-	}
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/*
-	printf("my realistic user id : %d(%s)\n", getuid(), getpwuid(getuid())->pw_name);
-	printf("my valid user id : %d(%s)\n", geteuid(), getpwuid(geteuid())->pw_name);
-	printf("my realistic group id : %d(%s)\n", getgid(), getgrgid(getgid())->gr_name);
-	printf("my valid group id : %d(%s)\n", getgid(), getgrgid(getuid())->gr_name);
-	*/
