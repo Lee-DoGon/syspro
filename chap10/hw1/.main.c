d557025851e5d66f3d3c440b0ba46d5844e36b7e
ydg6636 <144796016+Lee-DoGon@users.noreply.github.com>
2024-11-21 15:30:31 +0900

    Update .main.c

--- chap10/hw1/.main.c
+++ chap10/hw1/.main.c
@@ -1,66 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct node
-{
-	int data;
-	struct node *next;
+struct node {
+    int data;
+    struct node *next;
 };
 
-struct stack
-{
-	node *top;
+void push(struct node **top, int data) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+
+    if (new_node == NULL) {
+        perror("malloc failed");
+        exit(1);
+    }
+
+    new_node->data = data;
+    new_node->next = *top;
+    *top = new_node;
 }
 
+int pop(struct node **top) {
+    if (*top == NULL) {
+        return -1;
+    }
 
-void push(struct node *top, int data);
-{
-	node *data = (node*)malloc(sizeof(node));
-	temp->data = data;
-	temp->link = stack->top;
-	stack->top = temp;
+    struct node *temp = *top;
+    int popped_data = temp->data;
+
+    *top = (*top)->next;
+
+    free(temp);
+
+    return popped_data;
 }
 
+void print_stack(struct node *top) {
+    if (top == NULL) {
+        return;
+    }
 
-int pop(struct node *top)
-{
-	top->data[top->next];
-	top->next--;
-	retrun data;
+    struct node *p = top;
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
 }
 
+int main() {
+    struct node *top = NULL;
+    int num;
 
-int main()
-{
-	int num;
-	struct node *p, *head = NULL;
 
-	printf("num\n");
+    while (scanf("%d", &num) == 1 && num != -1) {
+        push(&top, num);
+    }
 
-	while (scanf("%d", &num) == 1)
-	{
-		p = (struct node*)malloc(sizeof(struct student));
+    printf("\nPrint stack:\n");
+    print_stack(top);
 
-		if (p == NULL)
-		{
-			perror("malloc");
-			exit(1);
-		}
-
-		p->data = data;
-		push(*p, data);
-	}
-
-	printf("\nPrint stack\n");
-
-	p = node;
-
-	while (p != NULL)
-	{
-		printf("%d\n", p->data);
-		p = p->node;
-	}
-
-	exit(0);
+    return 0;
 }
